acf78c216b634454dcc037ed571d3984f645cc08
AlieeldeenIbrahim <47504395+AlieeldeenIbrahim@users.noreply.github.com>
2019-03-21 22:13:01 +0200

    Add files via upload

---
+++ Partners (Sprint 2 modified)/model/partner.js
@@ -0,0 +1,70 @@
+const uuid = require('uuid')
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// The User Model
+const PartnerSchema = new Schema({
+    
+        name:{
+                type: String,
+                required: true
+        },
+        password:{
+                type: String,
+                required: true
+        },
+        email: {
+                type: String,
+                required: true
+        },
+        contact_info: {
+                type: String,
+                required: true    
+        },
+        registered_on: {
+                type: Date,
+                required: true  
+        } ,
+        com_reg_num: {
+                type: Number,
+                required: true
+        },
+        info: {
+                type: String,
+                required: true
+        },
+        signed: {
+                type: Boolean,
+                required: true
+        }, 
+        rating: {
+                type: Double,
+                required: true
+        },
+        submitted_tasks: {
+                type: [task],
+                required: true
+        },
+        notifications:{
+                type: [notification],
+            required: true
+        },
+        associates: {
+                type: [],
+                required: true
+        },
+        board_members: {
+                type: [],
+                required: true
+        },
+        events: {
+                type: [],
+                required: true
+        },
+        feedback: {
+                type: String,
+                required: true
+        }
+})
+module.exports = partner = mongoose.model('Partner', BookSchema)
