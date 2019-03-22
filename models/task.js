88c7448c24695ace56058e92f1bae74b0128c7dd
Mahmoud Lamei <46966140+MahmoudLamei@users.noreply.github.com>
2019-03-22 19:56:33 +0200

    Updated Task models with schema
    -Yasmeen and Lamei

--- models/task.js
+++ models/task.js
@@ -1,25 +1,71 @@
-const uuid = require('uuid')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-// The User Model
-class task {
-    constructor(approved_by,description,posted_by,Estimated_effort,Time_taken,Level_of_commitment,Experience_level,Monetary_compensation,Owner,Assigned_Consultancy,reviewed) {
-    this.approved_by = approved_by,
-    this.description = description,
-    this.posted_on = Date(),
-    this.posted_by = posted_by,
-    this.Estimated_effort = Estimated_effort,
-    this.Time_taken = Time_taken ,
-    this.Level_of_commitment = Level_of_commitment,
-    this.Experience_level = Experience_level,
-    this.Monetary_compensation = Monetary_compensation,
-    this.Owner = Owner,
-    this.Assigned_Consultancy = Assigned_Consultancy,
-    this.reviewed = reviewed,
-    this.Required_set_of_skills = [],
-    this.task_list = [],
-    this.applicant_list = [],
-    this.assigned_users = [],
-    this.id = uuid.v4();
-    };
-};
-module.exports = task
+//Creating the Schema 
+const taskSchema = new Schema({
+  approved_by: {
+    type: Admin,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  posted_on: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  posted_by: {
+    type: Admin,
+    required: true
+  },
+  Estimated_effort: {
+    type: String,
+    required: true
+  },
+  Time_taken: {
+    type: String,
+    required: true
+  },
+  Level_of_commitment: {
+    type: String,
+    required: true
+  },
+  Experience_level: {
+    type: String,
+    required: true
+  },
+  Monetary_compensation: {
+    type: Number,
+    required: true
+  },
+  Owner: {
+    type: Partner,
+    required: true
+  },
+  Assigned_Consultancy: {
+    type: Consultancy,
+    required: true
+  },
+  reviewed: {
+    type: Boolean,
+    required: true
+  },
+  Required_set_of_skills: {
+    type: [String],
+    required: true
+  },
+  task_list: {
+    type: [Task],
+    required: true
+  },
+  applicant_list: {
+    type: [User]
+  },
+  assigned_users: {
+    type: [User]
+  }
+});
+
+module.exports = Task = mongoose.model('tasks', TaskSchema)
