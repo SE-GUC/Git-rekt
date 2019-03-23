fbf0b0250d66b637207c9fa3ab37aa8b34d9847c
aharb44 <47505028+aharb44@users.noreply.github.com>
2019-03-23 21:45:58 +0200

    Revert "Add files via upload"

--- config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    //mongoURI: 'mongodb+srv://<gitrekt5@gmail.com>:<gitrekt10$>@c<host>/<GitRekt>',
-    mongoURI: 'mongodb+srv://Ali:Ali44@gitrekt-ohrak.mongodb.net/test?retryWrites=true',
-}
