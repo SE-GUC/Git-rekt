58b14e0058f8267303b679b315015ca9c725fdc1
aharb44 <47505028+aharb44@users.noreply.github.com>
2019-03-23 22:02:46 +0200

    Add files via upload

---
+++ CORRECT_SPRINT_2/models/Certificate.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// Create the schema
+const CertificateSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    prerequisites: {
+        type: String,
+        required: true
+    },
+})
+
+module.exports = Certificate = mongoose.model('certificates', CertificateSchema)
