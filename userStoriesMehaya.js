3db14a4991a37ce566bf749e60aa35952d16a021
MohamedMehaya <mehaya8@hotmail.com>
2019-03-31 23:48:03 +0200

    Add files via upload

--- userStoriesMehaya.js
+++ userStoriesMehaya.js
@@ -5,36 +5,33 @@ router.get('/api/consultancy/contactAdmin/:email', async (req,res) => {
 	res.send ({msg:'This is the adminEmail',data: adminEmail})
 })
 
-router.get('/api/admin/beInContactWithPartner/:id', async (req,res) => {
-	const id = req.params.id
-    const Partner = await partner.findById({id})
+
+router.get('/api/admin/beInContactWithPartner/:email', async (req,res) => {
+	const email = req.params.email
+    const Partner = await partner.findOne({email})
 	const partnerContactInfo = partner.contactInfo
 	Partner.notifications.push(admin.contactInfo)
 	res.send ({msg:'This is the Partner's contactInfo ',data: partnerContactInfo})
 })
 
 
-
-
-
-
 router.post('/api/admin/approveUserCertification/:CertificateApplications',async(req,res) =>{
-for(i=0;i<=certificateApplications.size();i++){
-	if(CertificateApplication.status=="done"){
-	id=CertificateApplication.id
-	const User = await Users.findById({id})
-	try{
-		const isValid = await certificateValidator.createValidation(req.body)
- 		if(isValidated.error) return res.status(400).send({error: isValidated.error.details[0].message})
-		const newCertificate = await certificate.create(req.body)
-		const newAdmin = await Admin.find({email})
-		newAdmin.approvedCertifications.push(newCertificate)
-		User.certifications.push(newCertificate)
-	}catch(error){
-		console.log(error)
+	for(i=0;i<=certificateApplications.size();i++){
+		if(CertificateApplication.status=="done"){
+		id=CertificateApplication.id
+		const User = await Users.findById({id})
+		try{
+			const isValid = await certificateValidator.createValidation(req.body)
+			if(isValidated.error) return res.status(400).send({error: isValidated.error.details[0].message})
+			const newCertificate = await certificate.create(req.body)
+			const newAdmin = await Admin.find({email})
+			newAdmin.approvedCertifications.push(newCertificate)
+			User.certifications.push(newCertificate)
+		}catch(error){
+			console.log(error)
+		}
+		}
 	}
-	}
-}
 
 })
 
