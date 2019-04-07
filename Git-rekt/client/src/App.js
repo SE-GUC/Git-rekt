361cc377ebac9b4367de7c5c09e1b21ede606c6b
aharb44 <47505028+aharb44@users.noreply.github.com>
2019-04-07 23:50:20 +0200

    Add files via upload
    Added react routes and login page
    Login button is not functional yet
    Register will route to registration page normally

---
+++ Git-rekt/client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+       <div className="App">
+          <div>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/userProfile" component={UserProfile} />
+            <Route exact path="/reviewCertificates" component={ReviewCertificates} />
+            <Route exact path="/displayCertificates" component={DisplayCertificates} />
+            <Route exact path="/partnerNotifications" component={PartnerNotifications} />
+          </div>
+       </div>
+       </Router>
+    );
+  }
+}
+
+export default App;
