898b409c75709ce4526c9cb3a1fedac0621dd9e7
hazemayman98 <47500197+hazemayman98@users.noreply.github.com>
2019-04-08 03:47:30 +0300

    Add files via upload

---
+++ hazem's code/validations/applicationValidations.js
@@ -0,0 +1,22 @@
+const Joi = require('joi')
+//const validator = require("../../validations/taskValidations");
+
+module.exports = {
+    createValidation: request => {
+        const createSchema = {
+            user: Joi.string().min(1).max(32).required(),
+            task: Joi.string().min(1).max(32).required()
+            
+        }
+        return Joi.validate(request, createSchema)
+    },
+
+    updateValidation: request => {
+        const updateSchema = {
+            user: Joi.string().min(1).max(32),
+            task: Joi.string().min(1).max(32),
+            date: Joi.date()
+        }
+        return Joi.validate(request, updateSchema)
+    }
+}
