b74c916c928109e46ed4fbf63f7eaaba042ef01e
MohamedMehaya <mehaya8@hotmail.com>
2019-04-22 21:54:06 +0200

    Add files via upload

---
+++ user can apply for certification.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+
+/*title,description,prerequisites,issuedBy,type*/
+
+
+export class userApplyCertification extends Component {
+    
+    async componentDidMount(){
+        const Body=await axios.put('http://localhost:3001/api/partner')
+        
+    }
+
+	
+
+     render() {
+     return (
+        <input
+            type="text"
+            title={this.state.title}
+            onChange={this.handleChange1}
+         />
+		
+		<input
+            type="text"
+            description={this.state.description}
+            onChange={this.handleChange2}
+         />
+		
+		<input
+            type="text"
+            prerequisites={this.state.prerequisites}
+            onChange={this.handleChange3}
+         />
+		 
+		<input
+            type="text"
+            issuedBy={this.state.issuedBy}
+            onChange={this.handleChange4}
+         /> 
+		 
+		<input
+            type="text"
+            type={this.state.type}
+            onChange={this.handleChange5}
+         /> 
+     );
+
+ }
+}
+
+export default userApplyCertification
